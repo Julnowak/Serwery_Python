d8d47b3141250993bec9010ab9cb8120badd4365
Julnowak <julnowak@student.agh.edu.pl>
2021-11-04 23:19:51 +0100

    Initial commit

---
+++ servers_skeleton.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from typing import Optional
+
+
+class Product:
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
+
+    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
+
+    def __eq__(self, other):
+        return self.price == other.price  # FIXME: zwróć odpowiednią wartość
+    
+    def __hash__(self):
+        return hash((self.name, self.price))
+
+
+class TooManyProductsFoundError:
+    # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
+    pass
+
+
+# FIXME: Każada z poniższych klas serwerów powinna posiadać:
+#   (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze,
+#   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
+#   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
+
+class ListServer:
+    pass
+
+
+class MapServer:
+    pass
+
+
+class Client:
+    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
+
+    def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
+        raise NotImplementedError()
