45df1e6c8c1415cd6ba0bcb755f62fc54fe4ba1d
Julnowak <julnowak@student.agh.edu.pl>
2021-11-07 22:20:09 +0100

    Initial commit

---
+++ servers_tests.py
@@ -0,0 +1,29 @@
+import unittest
+from collections import Counter
+
+from servers import ListServer, Product, Client, MapServer
+
+server_types = (ListServer, MapServer)
+
+
+class ServerTest(unittest.TestCase):
+
+    def test_get_entries_returns_proper_entries(self):
+        products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
+        for server_type in server_types:
+            server = server_type(products)
+            entries = server.get_entries(2)
+            self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
+
+
+class ClientTest(unittest.TestCase):
+    def test_total_price_for_normal_execution(self):
+        products = [Product('PP234', 2), Product('PP235', 3)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(5, client.get_total_price(2))
+
+
+if __name__ == '__main__':
+    unittest.main()
